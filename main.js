79afab2929cb6a78b8db8ff388b69fe6bf5c39f4
shyamp12041 <shyamp1204@utexas.edu>
2022-02-01 23:55:02 -0600

    Cycle 3 complete

---
+++ main.js
@@ -0,0 +1,8 @@
+var root = document.documentElement;
+const lists = document.querySelectorAll('.hs'); 
+
+lists.forEach(el => {
+  const listItems = el.querySelectorAll('li');
+  const n = el.children.length;
+  el.style.setProperty('--total', n);
+});
